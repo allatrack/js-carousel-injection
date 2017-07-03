943be638bce27b0b18bf13a9c085a043aea0c75c
Oleg Levitsky <levabd@gmail.com>
2017-07-03 14:49:06 +0300

    fix some bugs

--- carousel.js
+++ carousel.js
@@ -1,5 +1,3 @@
-'use strict';
-
 (function() {
 
     function closest(el, selector) {
@@ -38,9 +36,12 @@
         this.firstSlidePartialVisible = false;
         this.slides = [];
 
+        this.transitionalMoving = false;
+
         this.init = function() {
             document.getElementById("MarketGidComposite").innerHTML = tmpl("tmpl-carousel", _mgq[0][1]);
             document.getElementById("MPreload").style = "display: none;";
+            document.getElementById("MPreload").innerHTML = '';
             this.mgboxWidth = document.getElementById("MarketGidComposite").clientWidth - 2; // Borders
             this.slides = document.getElementsByClassName("mgline");
 
@@ -52,21 +53,33 @@
             document.getElementById("slider").addEventListener('transitionend', function() { that.decorateSlider(); }, false);
             document.getElementById("slider").addEventListener('msTransitionEnd', function() { that.decorateSlider(); }, false);
 
-            // Listeners for images
+            // Listeners and hover for images
             for (var i = 0; i < this.slides.length; i++) {
                 (function(i) {
                     that.slides[i].addEventListener('click', function() {
-                        if (that.slides[i].style.opacity < 1) {
+                        if (that.slides[i].getAttribute('data-opacity') < 1) {
                             that.move((that.slides[i].offsetLeft + that.currentCarouselShift < 0) ? 'right' : 'left');
-                            return false;
                         }
+                    });
+
+                    that.slides[i].addEventListener('mouseover', function() {
+                        if (that.slides[i].getAttribute('data-opacity') < 1) {
+                            that.slides[i].style.opacity = '0.6';
+                        }
+                    });
+
+                    that.slides[i].addEventListener('mouseout', function() {
+                        that.slides[i].style.opacity = '1';
                     });
                 })(i)
             }
 
+            // Hover listener
+
+            // Prevent links
             [].forEach.call(document.getElementsByClassName("mgline-link"), function(a) {
                 a.addEventListener('click', function() {
-                    if (closest(a, '.mgline').style.opacity < 1) {
+                    if (closest(a, '.mgline').getAttribute('data-opacity') < 1) {
                         event.preventDefault();
                         return false;
                     }
@@ -79,7 +92,7 @@
             this.currentLastVisibleIndex = 0;
 
             for (var i = 0; i < this.slides.length; i++) {
-                this.slides[i].style.opacity = '1';
+                this.slides[i].setAttribute('data-opacity', '1');
                 if (this.slides[i].isVisible()) {
                     if (this.currentFirstVisibleIndex === null) {
                         this.currentFirstVisibleIndex = i;
@@ -97,7 +110,7 @@
             if ((!this.firstSlidePartialVisible) && (this.currentFirstVisibleIndex < 1)) {
                 document.getElementById("slider-prev").style.display = 'none';
             } else {
-                this.slides[this.currentFirstVisibleIndex].style.opacity = '0.6';
+                this.slides[this.currentFirstVisibleIndex].setAttribute('data-opacity', '0.6');
                 document.getElementById("slider-prev").style.display = 'block';
             }
 
@@ -105,35 +118,58 @@
             if ((!this.lastSlidePartialVisible) && (this.currentLastVisibleIndex > this.slides.length - 2)) {
                 document.getElementById("slider-next").style.display = 'none';
             } else {
-                this.slides[this.currentLastVisibleIndex].style.opacity = '0.6';
+                this.slides[this.currentLastVisibleIndex].setAttribute('data-opacity', '0.6');
                 document.getElementById("slider-next").style.display = 'block';
             }
+
+            this.transitionalMoving = false;
         }
 
         this.move = function(side) {
-            if (side == 'left') {
-                // Very "left" position of the carousel
-                if ((!this.firstSlidePartialVisible) && (this.currentFirstVisibleIndex < 1)) {
-                    this.currentCarouselShift -= (233 - this.mgboxWidth % 233) / 2;
-                } else if (this.currentLastVisibleIndex > this.slides.length - 2) { // Most "right" position of the carousel
-                    var lastSlideRightBorder = this.slides[this.currentLastVisibleIndex].offsetLeft + this.slides[this.currentLastVisibleIndex].clientWidth + this.currentCarouselShift;
-                    this.currentCarouselShift -= lastSlideRightBorder - this.mgboxWidth + 6; //Margin + Border
-                } else {
-                    this.currentCarouselShift -= 233;
-                }
-            } else if (side == 'right') {
-                // Most "left" position of the carousel
-                if ((this.firstSlidePartialVisible) && (this.currentFirstVisibleIndex < 1)) {
-                    this.currentCarouselShift = 0;
-                    // Very "right" position of the carousel
-                } else if ((!this.lastSlidePartialVisible) && (this.currentLastVisibleIndex > this.slides.length - 2)) {
-                    this.currentCarouselShift += (233 - this.mgboxWidth % 233) / 2;
-                } else {
-                    this.currentCarouselShift += 233;
-                }
-            }
+            if (!this.transitionalMoving) {
+                if (side == 'left') {
+                    // Prevent redundant move
+                    if ((!this.lastSlidePartialVisible) && (this.currentLastVisibleIndex > this.slides.length - 2)) {
+                        return;
+                    }
 
-            document.getElementById("slider").style.left = this.currentCarouselShift + 'px';
+                    // Very "left" position of the carousel
+                    if ((!this.firstSlidePartialVisible) && (this.currentFirstVisibleIndex < 1)) {
+                        var shift = (233 - this.mgboxWidth % 233) / 2;
+                        if (shift < 5) { // Slides exactly fit container
+                            shift = 116
+                        }
+                        this.currentCarouselShift -= shift;
+                    } else if (this.currentLastVisibleIndex > this.slides.length - 2) { // Most "right" position of the carousel
+                        var lastSlideRightBorder = this.slides[this.currentLastVisibleIndex].offsetLeft + this.slides[this.currentLastVisibleIndex].clientWidth + this.currentCarouselShift;
+                        this.currentCarouselShift -= lastSlideRightBorder - this.mgboxWidth + 6; // Margin + Border
+                    } else {
+                        this.currentCarouselShift -= 233;
+                    }
+                } else if (side == 'right') {
+                    // Prevent redundant move
+                    if ((!this.firstSlidePartialVisible) && (this.currentFirstVisibleIndex < 1)) {
+                        return;
+                    }
+
+                    // Most "left" position of the carousel
+                    if ((this.firstSlidePartialVisible) && (this.currentFirstVisibleIndex < 1)) {
+                        this.currentCarouselShift = 0;
+                        // Very "right" position of the carousel
+                    } else if ((!this.lastSlidePartialVisible) && (this.currentLastVisibleIndex > this.slides.length - 2)) {
+                        var shift = (233 - this.mgboxWidth % 233) / 2;
+                        if (shift < 5) { // Slides exactly fit container
+                            shift = 116
+                        }
+                        this.currentCarouselShift += shift;
+                    } else {
+                        this.currentCarouselShift += 233;
+                    }
+                }
+
+                this.transitionalMoving = true;
+                document.getElementById("slider").style.left = this.currentCarouselShift + 'px';
+            }
         }
 
         this.slideNextPrevClick = function(element) {
